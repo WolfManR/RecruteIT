2a63aba4d3d46f12b0a9c608f6c62c4c2756759d
Tarxos <ftarxos@gmail.com>
2022-09-21 13:16:01 +0300

    prototype repository behavior

--- Data/ProductsRepository.cs
+++ Data/ProductsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Data.Contexts;
 using Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data;
 
@@ -14,21 +15,33 @@ public class ProductsRepository
 
     public IReadOnlyCollection<Product> FindProducts(string productName)
     {
-        return Array.Empty<Product>();
+        var products = _context.Products.AsNoTracking().Where(p => p.Name.Contains(productName)).ToList();
+        return products;
     }
 
     public Guid Add(Product product)
     {
-        return Guid.Empty;
+        _context.Products.Add(product);
+        _context.SaveChanges();
+        return product.Id;
     }
 
     public void Update(Product edited)
     {
+        var product = _context.Products.Find(edited.Id);
+        if (product is null) throw new KeyNotFoundException($"Can't update not existed product with id: {edited.Id}");
 
+        product.Name = edited.Name;
+        product.Description = edited.Description;
+
+        _context.SaveChanges();
     }
 
     public void Remove(Guid id)
     {
-
+        var product = _context.Products.Find(id);
+        if(product is null) throw new KeyNotFoundException($"Can't delete not existed product with id: {id}");
+        _context.Products.Remove(product);
+        _context.SaveChanges();
     }
 }
