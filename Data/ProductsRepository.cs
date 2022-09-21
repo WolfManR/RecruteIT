98b78fd9bebdbcacb116bb916d799b915dfc2bc5
Tarxos <ftarxos@gmail.com>
2022-09-21 13:03:44 +0300

    prototype repository

---
+++ Data/ProductsRepository.cs
@@ -0,0 +1,34 @@
+﻿using Data.Contexts;
+using Data.Entities;
+
+namespace Data;
+
+public class ProductsRepository
+{
+    private readonly TestDbContext _context;
+
+    public ProductsRepository(TestDbContext context)
+    {
+        _context = context;
+    }
+
+    public IReadOnlyCollection<Product> FindProducts(string productName)
+    {
+        return Array.Empty<Product>();
+    }
+
+    public Guid Add(Product product)
+    {
+        return Guid.Empty;
+    }
+
+    public void Update(Product edited)
+    {
+
+    }
+
+    public void Remove(Guid id)
+    {
+
+    }
+}
